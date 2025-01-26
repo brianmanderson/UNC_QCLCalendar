a8aa89a12bc2182c35838131f80d5c36c4689ba8
Brian Anderson <markba122@gmail.com>
2025-01-26 12:25:26 -0500

    update

---
+++ QCLCalendarMaker/QCLCalendarMaker/TreatmentClasses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QCLCalendarMaker
+{
+    public class TreatmentClass
+    {
+        public string Site { get; set; }           // e.g., "Lung", "Prostate", "Breast"
+        public int PlanningDays { get; set; }      // e.g., 4
+        public int ContouringDays { get; set; }    // e.g., 2
+    }
+    public class ModalityClass
+    {
+        public string Modality { get; set; }                // e.g., "3D", "IMRT", "VMAT"
+        public List<TreatmentClass> Treatments { get; set; } // A list of sites + day offsets
+    }
+}
