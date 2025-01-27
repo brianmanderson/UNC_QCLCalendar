36a7dbcf848e1c8d293ee30de5c17af011342ec0
brianmanderson <markba122@gmail.com>
2025-01-27 06:43:22 -0500

    treatmentclass

--- QCLCalendarMaker/QCLCalendarMaker/TreatmentClasses.cs
+++ QCLCalendarMaker/QCLCalendarMaker/TreatmentClasses.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Media;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+
 
 namespace QCLCalendarMaker
 {
