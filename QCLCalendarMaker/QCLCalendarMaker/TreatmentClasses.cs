7cc8e6841b05999dc4f305022cad494b461e94a4
brianmanderson <markba122@gmail.com>
2025-01-31 16:26:42 -0500

    updates

--- QCLCalendarMaker/QCLCalendarMaker/TreatmentClasses.cs
+++ QCLCalendarMaker/QCLCalendarMaker/TreatmentClasses.cs
@@ -20,9 +20,6 @@ namespace QCLCalendarMaker
     {
         public string Site { get; set; }           // e.g., "Lung", "Prostate", "Breast"
         public List<IndividualTask> SchedulingTasks { get; set; }
-        public int PlanningDays { get; set; }      // e.g., 4
-        public int ContouringDays { get; set; }    // e.g., 2
-        public int PlanningToStart { get; set; }
     }
     public class ModalityClass
     {
