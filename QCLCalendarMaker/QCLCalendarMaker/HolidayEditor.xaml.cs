75d7e22f0189693a8c76fda965d80559ac0f94ce
Brian Anderson <markba122@gmail.com>
2025-03-05 18:40:14 -0500

    updates

--- QCLCalendarMaker/QCLCalendarMaker/HolidayEditor.xaml.cs
+++ QCLCalendarMaker/QCLCalendarMaker/HolidayEditor.xaml.cs
@@ -8,13 +8,6 @@ using Newtonsoft.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using QCLCalendarMaker.Properties;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
